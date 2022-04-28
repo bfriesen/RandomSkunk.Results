30cb282cf5e9ced0ba13c2f78a568b3cadf2d599
Brian Friesen <brian.friesen@gmail.com>
2022-04-28 18:46:41 -0400

    Add asynchronous result extension methods

--- RandomSkunk.Results/GlobalUsings.cs
+++ RandomSkunk.Results/GlobalUsings.cs
@@ -3,4 +3,5 @@ global using System.Collections.Generic;
 global using System.Diagnostics.CodeAnalysis;
 global using System.Runtime.CompilerServices;
 global using System.Text;
+global using System.Threading;
 global using System.Threading.Tasks;
--- RandomSkunk.Results/ResultExtensionMethods.cs
+++ RandomSkunk.Results/ResultExtensionMethods.cs
@@ -273,6 +273,78 @@ public static class ResultExtensionMethods
     }
 
     /// <summary>
+    /// Maps <paramref name="result"/> to a new result using the specified <paramref name="mapAsync"/>
+    /// function. The map function is only evaluated if the target is a <c>some</c> result, and
+    /// the <see cref="Result{T}.Type"/> and <see cref="ResultBase.CallSite"/> of the new result
+    /// will always be the same as the target result.
+    /// </summary>
+    /// <typeparam name="T">The type of the return value of the operation.</typeparam>
+    /// <typeparam name="TResult">The type of the new result.</typeparam>
+    /// <param name="result">The target result.</param>
+    /// <param name="mapAsync">
+    /// A function that maps the value of the incoming result to the value of the outgoing result.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// The token to monitor for cancellation requests. The default value is
+    /// <see cref="CancellationToken.None"/>.
+    /// </param>
+    /// <returns>The mapped result.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// If <paramref name="result"/> is <see langword="null"/> or if <paramref name="mapAsync"/> is
+    /// <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// If <paramref name="mapAsync"/> returns <see langword="null"/> when evaluated.
+    /// </exception>
+    public static async Task<Result<TResult>> MapAsync<T, TResult>(
+        this Result<T> result,
+        Func<T, CancellationToken, Task<TResult>> mapAsync,
+        CancellationToken cancellationToken = default)
+    {
+        if (result is null) throw new ArgumentNullException(nameof(result));
+        if (mapAsync is null) throw new ArgumentNullException(nameof(mapAsync));
+
+        if (result.IsSuccess)
+        {
+            var mappedValue = await mapAsync(result.Value, cancellationToken)
+                ?? throw Exceptions.FunctionMustNotReturnNull(nameof(mapAsync));
+
+            return Result.Success(mappedValue, result.CallSite.MemberName, result.CallSite.FilePath, result.CallSite.LineNumber);
+        }
+
+        return Result.Fail<TResult>(
+            result.Error,
+            result.CallSite.MemberName,
+            result.CallSite.FilePath,
+            result.CallSite.LineNumber);
+    }
+
+    /// <summary>
+    /// Maps <paramref name="result"/> to a new result using the specified <paramref name="mapAsync"/>
+    /// function. The map function is only evaluated if the target is a <c>some</c> result, and
+    /// the <see cref="Result{T}.Type"/> and <see cref="ResultBase.CallSite"/> of the new result
+    /// will always be the same as the target result.
+    /// </summary>
+    /// <typeparam name="T">The type of the return value of the operation.</typeparam>
+    /// <typeparam name="TResult">The type of the new result.</typeparam>
+    /// <param name="result">The target result.</param>
+    /// <param name="mapAsync">
+    /// A function that maps the value of the incoming result to the value of the outgoing result.
+    /// </param>
+    /// <returns>The mapped result.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// If <paramref name="result"/> is <see langword="null"/> or if <paramref name="mapAsync"/> is
+    /// <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// If <paramref name="mapAsync"/> returns <see langword="null"/> when evaluated.
+    /// </exception>
+    public static Task<Result<TResult>> MapAsync<T, TResult>(
+        this Result<T> result,
+        Func<T, Task<TResult>> mapAsync) =>
+        result.MapAsync((value, cancellationToken) => mapAsync(value), default);
+
+    /// <summary>
     /// Maps <paramref name="result"/> to a new result using the specified <paramref name="map"/>
     /// function. The map function is only evaluated if the target is a <c>some</c> result, and
     /// the <see cref="Result{T}.Type"/> and <see cref="ResultBase.CallSite"/> of the new result
@@ -316,6 +388,81 @@ public static class ResultExtensionMethods
     }
 
     /// <summary>
+    /// Maps <paramref name="result"/> to a new result using the specified <paramref name="mapAsync"/>
+    /// function. The map function is only evaluated if the target is a <c>some</c> result, and
+    /// the <see cref="MaybeResult{T}.Type"/> and <see cref="ResultBase.CallSite"/> of the new result
+    /// will always be the same as the target result.
+    /// </summary>
+    /// <typeparam name="T">The type of the return value of the operation.</typeparam>
+    /// <typeparam name="TResult">The type of the new result.</typeparam>
+    /// <param name="result">The target result.</param>
+    /// <param name="mapAsync">
+    /// A function that maps the value of the incoming result to the value of the outgoing result.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// The token to monitor for cancellation requests. The default value is
+    /// <see cref="CancellationToken.None"/>.
+    /// </param>
+    /// <returns>The mapped result.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// If <paramref name="result"/> is <see langword="null"/> or if <paramref name="mapAsync"/> is
+    /// <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// If <paramref name="mapAsync"/> returns <see langword="null"/> when evaluated.
+    /// </exception>
+    public static async Task<MaybeResult<TResult>> MapAsync<T, TResult>(
+        this MaybeResult<T> result,
+        Func<T, CancellationToken, Task<TResult>> mapAsync,
+        CancellationToken cancellationToken = default)
+    {
+        if (result is null) throw new ArgumentNullException(nameof(result));
+        if (mapAsync is null) throw new ArgumentNullException(nameof(mapAsync));
+
+        if (result.IsSome)
+        {
+            var mappedValue = await mapAsync(result.Value, cancellationToken)
+                ?? throw Exceptions.FunctionMustNotReturnNull(nameof(mapAsync));
+
+            return MaybeResult.Some(mappedValue, result.CallSite.MemberName, result.CallSite.FilePath, result.CallSite.LineNumber);
+        }
+
+        if (result.IsNone)
+            return MaybeResult.None<TResult>(result.CallSite.MemberName, result.CallSite.FilePath, result.CallSite.LineNumber);
+
+        return MaybeResult.Fail<TResult>(
+            result.Error,
+            result.CallSite.MemberName,
+            result.CallSite.FilePath,
+            result.CallSite.LineNumber);
+    }
+
+    /// <summary>
+    /// Maps <paramref name="result"/> to a new result using the specified <paramref name="mapAsync"/>
+    /// function. The map function is only evaluated if the target is a <c>some</c> result, and
+    /// the <see cref="MaybeResult{T}.Type"/> and <see cref="ResultBase.CallSite"/> of the new result
+    /// will always be the same as the target result.
+    /// </summary>
+    /// <typeparam name="T">The type of the return value of the operation.</typeparam>
+    /// <typeparam name="TResult">The type of the new result.</typeparam>
+    /// <param name="result">The target result.</param>
+    /// <param name="mapAsync">
+    /// A function that maps the value of the incoming result to the value of the outgoing result.
+    /// </param>
+    /// <returns>The mapped result.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// If <paramref name="result"/> is <see langword="null"/> or if <paramref name="mapAsync"/> is
+    /// <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// If <paramref name="mapAsync"/> returns <see langword="null"/> when evaluated.
+    /// </exception>
+    public static Task<MaybeResult<TResult>> MapAsync<T, TResult>(
+        this MaybeResult<T> result,
+        Func<T, Task<TResult>> mapAsync) =>
+        result.MapAsync((value, cancellationToken) => mapAsync(value), default);
+
+    /// <summary>
     /// Maps <paramref name="result"/> to a another result using the specified
     /// <paramref name="flatMap"/> function. The flat map function is only evaluated if the target
     /// is a <c>success</c> result. If the target is a <c>fail</c> result, the error and callsite
@@ -354,6 +501,78 @@ public static class ResultExtensionMethods
             result.CallSite.FilePath,
             result.CallSite.LineNumber);
     }
+
+    /// <summary>
+    /// Maps <paramref name="result"/> to a another result using the specified
+    /// <paramref name="flatMapAsync"/> function. The flat map function is only evaluated if the target
+    /// is a <c>success</c> result. If the target is a <c>fail</c> result, the error and callsite
+    /// are propagated to the returned <c>fail</c> result.
+    /// </summary>
+    /// <typeparam name="T">The type of the return value of the operation.</typeparam>
+    /// <typeparam name="TResult">The type of the new result.</typeparam>
+    /// <param name="result">The target result.</param>
+    /// <param name="flatMapAsync">
+    /// A function that maps the value of the incoming result to the value of the outgoing result.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// The token to monitor for cancellation requests. The default value is
+    /// <see cref="CancellationToken.None"/>.
+    /// </param>
+    /// <returns>The flat mapped result.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// If <paramref name="result"/> is <see langword="null"/> or if <paramref name="flatMapAsync"/> is
+    /// <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// If <paramref name="flatMapAsync"/> returns <see langword="null"/> when evaluated.
+    /// </exception>
+    public static async Task<Result<TResult>> FlatMapAsync<T, TResult>(
+        this Result<T> result,
+        Func<T, CancellationToken, Task<Result<TResult>>> flatMapAsync,
+        CancellationToken cancellationToken = default)
+    {
+        if (result is null) throw new ArgumentNullException(nameof(result));
+        if (flatMapAsync is null) throw new ArgumentNullException(nameof(flatMapAsync));
+
+        if (result.IsSuccess)
+        {
+            var mappedValue = await flatMapAsync(result.Value, cancellationToken)
+                ?? throw Exceptions.FunctionMustNotReturnNull(nameof(flatMapAsync));
+
+            return mappedValue;
+        }
+
+        return Result.Fail<TResult>(
+            result.Error,
+            result.CallSite.MemberName,
+            result.CallSite.FilePath,
+            result.CallSite.LineNumber);
+    }
+
+    /// <summary>
+    /// Maps <paramref name="result"/> to a another result using the specified
+    /// <paramref name="flatMapAsync"/> function. The flat map function is only evaluated if the target
+    /// is a <c>success</c> result. If the target is a <c>fail</c> result, the error and callsite
+    /// are propagated to the returned <c>fail</c> result.
+    /// </summary>
+    /// <typeparam name="T">The type of the return value of the operation.</typeparam>
+    /// <typeparam name="TResult">The type of the new result.</typeparam>
+    /// <param name="result">The target result.</param>
+    /// <param name="flatMapAsync">
+    /// A function that maps the value of the incoming result to the value of the outgoing result.
+    /// </param>
+    /// <returns>The flat mapped result.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// If <paramref name="result"/> is <see langword="null"/> or if <paramref name="flatMapAsync"/> is
+    /// <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// If <paramref name="flatMapAsync"/> returns <see langword="null"/> when evaluated.
+    /// </exception>
+    public static Task<Result<TResult>> FlatMapAsync<T, TResult>(
+        this Result<T> result,
+        Func<T, Task<Result<TResult>>> flatMapAsync) =>
+        result.FlatMapAsync((value, cancellationToken) => flatMapAsync(value), default);
 
     /// <summary>
     /// Maps <paramref name="result"/> to a another result using the specified
@@ -398,6 +617,83 @@ public static class ResultExtensionMethods
             result.CallSite.FilePath,
             result.CallSite.LineNumber);
     }
+
+    /// <summary>
+    /// Maps <paramref name="result"/> to a another result using the specified
+    /// <paramref name="flatMapAsync"/> function. The flat map function is only evaluated if the target
+    /// is a <c>success</c> result. If the target is a <c>none</c> or <c>fail</c> result, the
+    /// callsite is propagated to the returned <c>none</c> or <c>fail</c> result. If the target is
+    /// a <c>fail</c> result, the error is propagated as well.
+    /// </summary>
+    /// <typeparam name="T">The type of the return value of the operation.</typeparam>
+    /// <typeparam name="TResult">The type of the new result.</typeparam>
+    /// <param name="result">The target result.</param>
+    /// <param name="flatMapAsync">
+    /// A function that maps the value of the incoming result to the value of the outgoing result.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// The token to monitor for cancellation requests. The default value is
+    /// <see cref="CancellationToken.None"/>.
+    /// </param>
+    /// <returns>The flat mapped result.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// If <paramref name="result"/> is <see langword="null"/> or if <paramref name="flatMapAsync"/> is
+    /// <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// If <paramref name="flatMapAsync"/> returns <see langword="null"/> when evaluated.
+    /// </exception>
+    public static async Task<MaybeResult<TResult>> FlatMapAsync<T, TResult>(
+        this MaybeResult<T> result,
+        Func<T, CancellationToken, Task<MaybeResult<TResult>>> flatMapAsync,
+        CancellationToken cancellationToken)
+    {
+        if (result is null) throw new ArgumentNullException(nameof(result));
+        if (flatMapAsync is null) throw new ArgumentNullException(nameof(flatMapAsync));
+
+        if (result.IsSome)
+        {
+            var mappedValue = await flatMapAsync(result.Value, cancellationToken)
+                ?? throw Exceptions.FunctionMustNotReturnNull(nameof(flatMapAsync));
+
+            return mappedValue;
+        }
+
+        if (result.IsNone)
+            return MaybeResult.None<TResult>(result.CallSite.MemberName, result.CallSite.FilePath, result.CallSite.LineNumber);
+
+        return MaybeResult.Fail<TResult>(
+            result.Error,
+            result.CallSite.MemberName,
+            result.CallSite.FilePath,
+            result.CallSite.LineNumber);
+    }
+
+    /// <summary>
+    /// Maps <paramref name="result"/> to a another result using the specified
+    /// <paramref name="flatMapAsync"/> function. The flat map function is only evaluated if the target
+    /// is a <c>success</c> result. If the target is a <c>none</c> or <c>fail</c> result, the
+    /// callsite is propagated to the returned <c>none</c> or <c>fail</c> result. If the target is
+    /// a <c>fail</c> result, the error is propagated as well.
+    /// </summary>
+    /// <typeparam name="T">The type of the return value of the operation.</typeparam>
+    /// <typeparam name="TResult">The type of the new result.</typeparam>
+    /// <param name="result">The target result.</param>
+    /// <param name="flatMapAsync">
+    /// A function that maps the value of the incoming result to the value of the outgoing result.
+    /// </param>
+    /// <returns>The flat mapped result.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// If <paramref name="result"/> is <see langword="null"/> or if <paramref name="flatMapAsync"/> is
+    /// <see langword="null"/>.
+    /// </exception>
+    /// <exception cref="ArgumentException">
+    /// If <paramref name="flatMapAsync"/> returns <see langword="null"/> when evaluated.
+    /// </exception>
+    public static Task<MaybeResult<TResult>> FlatMapAsync<T, TResult>(
+        this MaybeResult<T> result,
+        Func<T, Task<MaybeResult<TResult>>> flatMapAsync) =>
+        result.FlatMapAsync((value, cancellationToken) => flatMapAsync(value), default);
 
     /// <summary>
     /// Flattens the nested result.
@@ -462,4 +758,64 @@ public static class ResultExtensionMethods
 
         return result;
     }
+
+    /// <summary>
+    /// Filter the specified result into a <c>none</c> result if it is a <c>some</c> result and the
+    /// <paramref name="filterAsync"/> function evaluates to <see langword="false"/>. The callsite is
+    /// always propagated to the returned result.
+    /// </summary>
+    /// <typeparam name="T">The type of the return value of the operation.</typeparam>
+    /// <param name="result">The target result.</param>
+    /// <param name="filterAsync">
+    /// A function that filters a <c>some</c> result into a <c>none</c> result by returning
+    /// <see langword="false"/>.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// The token to monitor for cancellation requests. The default value is
+    /// <see cref="CancellationToken.None"/>.
+    /// </param>
+    /// <returns>The filtered result.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// If <paramref name="result"/> is <see langword="null"/> or if <paramref name="filterAsync"/> is
+    /// <see langword="null"/>.
+    /// </exception>
+    public static async Task<MaybeResult<T>> FilterAsync<T>(
+        this MaybeResult<T> result,
+        Func<T, CancellationToken, Task<bool>> filterAsync,
+        CancellationToken cancellationToken)
+    {
+        if (result is null) throw new ArgumentNullException(nameof(result));
+        if (filterAsync is null) throw new ArgumentNullException(nameof(filterAsync));
+
+        if (result.IsSome)
+        {
+            if (await filterAsync(result.Value, cancellationToken))
+                return result;
+
+            return MaybeResult.None<T>(result.CallSite.MemberName, result.CallSite.FilePath, result.CallSite.LineNumber);
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Filter the specified result into a <c>none</c> result if it is a <c>some</c> result and the
+    /// <paramref name="filterAsync"/> function evaluates to <see langword="false"/>. The callsite is
+    /// always propagated to the returned result.
+    /// </summary>
+    /// <typeparam name="T">The type of the return value of the operation.</typeparam>
+    /// <param name="result">The target result.</param>
+    /// <param name="filterAsync">
+    /// A function that filters a <c>some</c> result into a <c>none</c> result by returning
+    /// <see langword="false"/>.
+    /// </param>
+    /// <returns>The filtered result.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// If <paramref name="result"/> is <see langword="null"/> or if <paramref name="filterAsync"/> is
+    /// <see langword="null"/>.
+    /// </exception>
+    public static Task<MaybeResult<T>> FilterAsync<T>(
+        this MaybeResult<T> result,
+        Func<T, Task<bool>> filterAsync) =>
+        result.FilterAsync((value, cancellationToken) => filterAsync(value), default);
 }
