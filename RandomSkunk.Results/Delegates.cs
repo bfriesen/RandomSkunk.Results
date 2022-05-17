686fec8ad86168ec89117ed4a20c8a4ad1eb826d
Brian Friesen <brian.friesen@gmail.com>
2022-05-17 17:45:59 -0400

    Add validation to Delegates helper methods

--- RandomSkunk.Results/Delegates.cs
+++ RandomSkunk.Results/Delegates.cs
@@ -11,7 +11,8 @@ public static class Delegates
     /// </summary>
     /// <param name="action">The <c>Action</c> delegate to return.</param>
     /// <returns>The <c>Action</c> delegate.</returns>
-    public static Action Action(Action action) => action;
+    /// <exception cref="ArgumentNullException">If <paramref name="action"/> is <see langword="null"/>.</exception>
+    public static Action Action(Action action) => action ?? throw new ArgumentNullException(nameof(action));
 
     /// <summary>
     /// Returns the specified <c>Func&lt;T&gt;</c> delegate.
@@ -19,14 +20,25 @@ public static class Delegates
     /// <typeparam name="T">The type of the return value of the method that the delegate encapsulates.</typeparam>
     /// <param name="func">The <c>Func&lt;T&gt;</c> delegate to return.</param>
     /// <returns>The <c>Func&lt;T&gt;</c> delegate.</returns>
-    public static Func<T> Func<T>(Func<T> func) => func;
+    /// <exception cref="ArgumentException">
+    /// If <typeparamref name="T"/> is <see cref="Task"/> or <see cref="Task{TResult}"/>.
+    /// </exception>
+    /// <exception cref="ArgumentNullException">If <paramref name="func"/> is <see langword="null"/>.</exception>
+    public static Func<T> Func<T>(Func<T> func)
+    {
+        if (typeof(Task).IsAssignableFrom(typeof(T)))
+            throw new ArgumentException("Generic argument T cannot be a Task. Call the AsyncAction or AsyncFunc<T> instead.");
+
+        return func ?? throw new ArgumentNullException(nameof(func));
+    }
 
     /// <summary>
     /// Returns the specified <c>AsyncAction</c> delegate.
     /// </summary>
     /// <param name="asyncAction">The <c>AsyncAction</c> delegate to return.</param>
     /// <returns>The <c>AsyncAction</c> delegate.</returns>
-    public static AsyncAction AsyncAction(AsyncAction asyncAction) => asyncAction;
+    /// <exception cref="ArgumentNullException">If <paramref name="asyncAction"/> is <see langword="null"/>.</exception>
+    public static AsyncAction AsyncAction(AsyncAction asyncAction) => asyncAction ?? throw new ArgumentNullException(nameof(asyncAction));
 
     /// <summary>
     /// Returns the specified <c>AsyncFunc&lt;T&gt;</c> delegate.
@@ -34,5 +46,6 @@ public static class Delegates
     /// <typeparam name="T">The type of the return value of the method that the delegate encapsulates.</typeparam>
     /// <param name="asyncFunc">The <c>AsyncFunc&lt;T&gt;</c> delegate to return.</param>
     /// <returns>The <c>AsyncFunc&lt;T&gt;</c> delegate.</returns>
-    public static AsyncFunc<T> AsyncFunc<T>(AsyncFunc<T> asyncFunc) => asyncFunc;
+    /// <exception cref="ArgumentNullException">If <paramref name="asyncFunc"/> is <see langword="null"/>.</exception>
+    public static AsyncFunc<T> AsyncFunc<T>(AsyncFunc<T> asyncFunc) => asyncFunc ?? throw new ArgumentNullException(nameof(asyncFunc));
 }
