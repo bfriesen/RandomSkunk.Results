5c46cabaddff7f7f3f5ea6a08d4beba8a2a78e38
Brian Friesen <brian.friesen@gmail.com>
2022-05-23 17:23:23 -0400

    Add missing OnSuccess extension methods

--- RandomSkunk.Results/ResultExtensions.OnSuccess.cs
+++ RandomSkunk.Results/ResultExtensions.OnSuccess.cs
@@ -12,6 +12,36 @@ public static partial class ResultExtensions
     /// <param name="source">The source result.</param>
     /// <param name="onSuccess">A callback function to invoke if the source is a <c>Success</c> result.</param>
     /// <returns>The <paramref name="source"/> result.</returns>
+    public static Result OnSuccess(this Result source, Action onSuccess)
+    {
+        if (source.IsSuccess)
+            onSuccess();
+
+        return source;
+    }
+
+    /// <summary>
+    /// Invokes the <paramref name="onSuccess"/> function if <paramref name="source"/> is a <c>Success</c> result.
+    /// </summary>
+    /// <typeparam name="T">The type of the source result value.</typeparam>
+    /// <param name="source">The source result.</param>
+    /// <param name="onSuccess">A callback function to invoke if the source is a <c>Success</c> result.</param>
+    /// <returns>The <paramref name="source"/> result.</returns>
+    public static async Task<Result> OnSuccessAsync(this Result source, Func<Task> onSuccess)
+    {
+        if (source.IsSuccess)
+            await onSuccess();
+
+        return source;
+    }
+
+    /// <summary>
+    /// Invokes the <paramref name="onSuccess"/> function if <paramref name="source"/> is a <c>Success</c> result.
+    /// </summary>
+    /// <typeparam name="T">The type of the source result value.</typeparam>
+    /// <param name="source">The source result.</param>
+    /// <param name="onSuccess">A callback function to invoke if the source is a <c>Success</c> result.</param>
+    /// <returns>The <paramref name="source"/> result.</returns>
     public static Result<T> OnSuccess<T>(this Result<T> source, Action<T> onSuccess)
     {
         if (source.IsSuccess)
