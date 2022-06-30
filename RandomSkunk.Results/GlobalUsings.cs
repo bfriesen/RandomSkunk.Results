a445af3f1c3a3116fccac981427d34101798a010
Brian Friesen <brian.friesen@gmail.com>
2022-06-30 16:14:29 -0400

    Generate code for result tuple extensions
    Generate the code for result tuple extension methods instead of doing
    copy/paste/edit on thousands of lines of code.

--- RandomSkunk.Results/GlobalUsings.cs
+++ RandomSkunk.Results/GlobalUsings.cs
@@ -6,6 +6,7 @@ global using System.Collections.ObjectModel;
 global using System.ComponentModel;
 global using System.Diagnostics;
 global using System.Diagnostics.CodeAnalysis;
+global using System.Linq;
 global using System.Reflection;
 global using System.Runtime.CompilerServices;
 global using System.Text;
