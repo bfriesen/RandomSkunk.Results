b326bb5c9c9e029266b153ab1e032479e078f49f
Brian Friesen <brian.friesen@gmail.com>
2022-09-09 17:41:42 -0400

    Add TryGetError method for each result type

---
+++ RandomSkunk.Results.UnitTests/TryGetError_methods.cs
@@ -0,0 +1,93 @@
+namespace RandomSkunk.Results.UnitTests;
+
+public class TryGetError_methods
+{
+    public class For_Result
+    {
+        [Fact]
+        public void Given_Fail_result_Returns_true_with_out_parameter_set()
+        {
+            var expectedError = new Error();
+            var result = Result.Fail(expectedError);
+
+            var returnValue = result.TryGetError(out var error);
+
+            returnValue.Should().BeTrue();
+            error.Should().BeSameAs(expectedError);
+        }
+
+        [Fact]
+        public void Given_Success_result_Returns_false_with_out_parameter_null()
+        {
+            var result = Result.Success();
+
+            var returnValue = result.TryGetError(out var error);
+
+            returnValue.Should().BeFalse();
+            error.Should().BeNull();
+        }
+    }
+
+    public class For_Result_of_T
+    {
+        [Fact]
+        public void Given_Fail_result_Returns_true_with_out_parameter_set()
+        {
+            var expectedError = new Error();
+            var result = Result<int>.Fail(expectedError);
+
+            var returnValue = result.TryGetError(out var error);
+
+            returnValue.Should().BeTrue();
+            error.Should().BeSameAs(expectedError);
+        }
+
+        [Fact]
+        public void Given_Success_result_Returns_false_with_out_parameter_null()
+        {
+            var result = Result<int>.Success(123);
+
+            var returnValue = result.TryGetError(out var error);
+
+            returnValue.Should().BeFalse();
+            error.Should().BeNull();
+        }
+    }
+
+    public class For_Maybe_of_T
+    {
+        [Fact]
+        public void Given_Fail_result_Returns_true_with_out_parameter_set()
+        {
+            var expectedError = new Error();
+            var result = Maybe<int>.Fail(expectedError);
+
+            var returnValue = result.TryGetError(out var error);
+
+            returnValue.Should().BeTrue();
+            error.Should().BeSameAs(expectedError);
+        }
+
+        [Fact]
+        public void Given_Success_result_Returns_false_with_out_parameter_null()
+        {
+            var result = Maybe<int>.Success(123);
+
+            var returnValue = result.TryGetError(out var error);
+
+            returnValue.Should().BeFalse();
+            error.Should().BeNull();
+        }
+
+        [Fact]
+        public void Given_None_result_Returns_false_with_out_parameter_null()
+        {
+            var result = Maybe<int>.None();
+
+            var returnValue = result.TryGetError(out var error);
+
+            returnValue.Should().BeFalse();
+            error.Should().BeNull();
+        }
+    }
+}
