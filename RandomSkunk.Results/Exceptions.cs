51d1f85415687f6677c624b2e5b98b7d7b114783
Brian Friesen <brian.friesen@gmail.com>
2023-01-06 22:45:46 -0500

    Fix doc comments, remove unused code

--- RandomSkunk.Results/Exceptions.cs
+++ RandomSkunk.Results/Exceptions.cs
@@ -5,7 +5,6 @@ internal static class Exceptions
     public const string CannotAccessErrorUnlessFailMessage = "Result error cannot be accessed unless it is a Fail result.";
     public const string CannotAccessErrorUnlessNonSuccessMessage = "Result error cannot be accessed unless it is a non-Success result.";
     public const string CannotAccessValueUnlessSuccessMessage = "Result value cannot be accessed unless it is a Success result.";
-    public const string FunctionMustNotReturnNullMessage = "Function must not return a null value.";
 
     public static InvalidStateException CannotAccessErrorUnlessFail() => new(CannotAccessErrorUnlessFailMessage);
 
