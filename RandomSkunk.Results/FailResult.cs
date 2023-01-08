5140ed479784456adca0f52faac52ff5681fe66e
Brian Friesen <brian.friesen@gmail.com>
2023-01-07 19:52:26 -0500

    Add ability to set multiple callbacks

--- RandomSkunk.Results/FailResult.cs
+++ RandomSkunk.Results/FailResult.cs
@@ -18,15 +18,42 @@ public static class FailResult
     /// Sets the callback function that will be invoked whenever a <c>Fail</c> result is created.
     /// </summary>
     /// <param name="onFailCallback">The callback function.</param>
-    public static void SetCallbackFunction(Action<Error> onFailCallback) =>
+    public static void SetCallbackFunction(Action<Error>? onFailCallback) =>
         _callback = onFailCallback;
 
     /// <summary>
     /// Sets the error replacement function that will be invoked whenever a <c>Fail</c> result is created.
     /// </summary>
     /// <param name="onFailReplaceError">The error replacement function.</param>
-    public static void SetReplaceErrorFunction(Func<Error, Error> onFailReplaceError) =>
+    public static void SetReplaceErrorFunction(Func<Error, Error>? onFailReplaceError) =>
         _replaceError = onFailReplaceError;
+
+    /// <summary>
+    /// Adds a callback function that will be invoked whenever a <c>Fail</c> result is created.
+    /// </summary>
+    /// <param name="onFailCallback">The callback function.</param>
+    public static void AddCallbackFunction(Action<Error> onFailCallback) =>
+        _callback += onFailCallback;
+
+    /// <summary>
+    /// Adds an error replacement function that will be invoked whenever a <c>Fail</c> result is created.
+    /// </summary>
+    /// <param name="onFailReplaceError">The error replacement function.</param>
+    public static void AddReplaceErrorFunction(Func<Error, Error> onFailReplaceError)
+    {
+        if (onFailReplaceError is null)
+            return;
+
+        var replaceError = _replaceError;
+        if (replaceError is null)
+        {
+            _replaceError = onFailReplaceError;
+        }
+        else
+        {
+            _replaceError = error => onFailReplaceError(replaceError(error));
+        }
+    }
 
     internal static Error InvokeReplaceErrorIfSet(Error error)
     {
