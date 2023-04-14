89795e333a56c24a2a78246b365ce74fe968759b
Brian Friesen <brian.friesen@gmail.com>
2023-04-14 15:19:28 -0400

    Adjust doc comments

--- RandomSkunk.Results/Operations/ToFailIf.cs
+++ RandomSkunk.Results/Operations/ToFailIf.cs
@@ -8,7 +8,8 @@ public partial struct Result
     /// result; otherwise returns the same result.
     /// </summary>
     /// <param name="predicate">The delegate that determines whether to return a <c>Fail</c> result.</param>
-    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error.</param>
+    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error. If <see langword="null"/>, a
+    ///     generic error is used.</param>
     /// <returns>A <c>Fail</c> result if <paramref name="predicate"/> returned <see langword="true"/>, or the same result if it
     ///     did not.</returns>
     public Result ToFailIf(Func<bool> predicate, Func<Error>? getError = null)
@@ -30,7 +31,8 @@ public partial struct Result<T>
     /// result; otherwise returns the same result.
     /// </summary>
     /// <param name="predicate">The delegate that determines whether to return a <c>Fail</c> result.</param>
-    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error.</param>
+    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error. If <see langword="null"/>, a
+    ///     generic error is used.</param>
     /// <returns>A <c>Fail</c> result if <paramref name="predicate"/> returned <see langword="true"/>, or the same result if it
     ///     did not.</returns>
     public Result<T> ToFailIf(Func<T, bool> predicate, Func<T, Error>? getError = null)
@@ -52,7 +54,8 @@ public partial struct Maybe<T>
     /// result; otherwise returns the same result.
     /// </summary>
     /// <param name="predicate">The delegate that determines whether to return a <c>Fail</c> result.</param>
-    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error.</param>
+    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error. If <see langword="null"/>, a
+    ///     generic error is used.</param>
     /// <returns>A <c>Fail</c> result if <paramref name="predicate"/> returned <see langword="true"/>, or the same result if it
     ///     did not.</returns>
     public Maybe<T> ToFailIf(Func<T, bool> predicate, Func<T, Error>? getError = null)
@@ -68,7 +71,8 @@ public partial struct Maybe<T>
     /// <summary>
     /// Gets a <c>Fail</c> result if the current result is <c>None</c>; otherwise returns the same result.
     /// </summary>
-    /// <param name="getError">An optional fuction that gets the <c>Fail</c> result's error.</param>
+    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error. If <see langword="null"/>, a
+    ///     generic error is used.</param>
     /// <returns>A <c>Fail</c> result if the current result is <c>None</c>, or the same result if it is <c>Success</c> or
     ///     <c>Fail</c>.</returns>
     public Maybe<T> ToFailIfNone(Func<Error>? getError = null)
@@ -89,7 +93,8 @@ public static partial class ResultExtensions
     /// </summary>
     /// <param name="sourceResult">The source result.</param>
     /// <param name="predicate">The delegate that determines whether to return a <c>Fail</c> result.</param>
-    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error.</param>
+    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error. If <see langword="null"/>, a
+    ///     generic error is used.</param>
     /// <returns>A <c>Fail</c> result if <paramref name="predicate"/> returned <see langword="true"/>, or the same result if it
     ///     did not.</returns>
     public static async Task<Result> ToFailIf(this Task<Result> sourceResult, Func<bool> predicate, Func<Error>? getError = null) =>
@@ -102,7 +107,8 @@ public static partial class ResultExtensions
     /// <typeparam name="T">The type of the source result value.</typeparam>
     /// <param name="sourceResult">The source result.</param>
     /// <param name="predicate">The delegate that determines whether to return a <c>Fail</c> result.</param>
-    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error.</param>
+    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error. If <see langword="null"/>, a
+    ///     generic error is used.</param>
     /// <returns>A <c>Fail</c> result if <paramref name="predicate"/> returned <see langword="true"/>, or the same result if it
     ///     did not.</returns>
     public static async Task<Result<T>> ToFailIf<T>(this Task<Result<T>> sourceResult, Func<T, bool> predicate, Func<T, Error>? getError = null) =>
@@ -115,7 +121,8 @@ public static partial class ResultExtensions
     /// <typeparam name="T">The type of the source result value.</typeparam>
     /// <param name="sourceResult">The source result.</param>
     /// <param name="predicate">The delegate that determines whether to return a <c>Fail</c> result.</param>
-    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error.</param>
+    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error. If <see langword="null"/>, a
+    ///     generic error is used.</param>
     /// <returns>A <c>Fail</c> result if <paramref name="predicate"/> returned <see langword="true"/>, or the same result if it
     ///     did not.</returns>
     public static async Task<Maybe<T>> ToFailIf<T>(this Task<Maybe<T>> sourceResult, Func<T, bool> predicate, Func<T, Error>? getError = null) =>
@@ -126,7 +133,8 @@ public static partial class ResultExtensions
     /// </summary>
     /// <typeparam name="T">The type of the source result value.</typeparam>
     /// <param name="sourceResult">The source result.</param>
-    /// <param name="getError">An optional fuction that gets the <c>Fail</c> result's error.</param>
+    /// <param name="getError">An optional function that gets the <c>Fail</c> result's error. If <see langword="null"/>, a
+    ///     generic error is used.</param>
     /// <returns>A <c>Fail</c> result if the current result is <c>None</c>, or the same result if it is <c>Success</c> or
     ///     <c>Fail</c>.</returns>
     public static async Task<Maybe<T>> ToFailIfNone<T>(this Task<Maybe<T>> sourceResult, Func<Error>? getError = null) =>
